297253fe22434cb7f217138ba67ec2f00e481e52
Shelly Eldman <shelyeldman@gmail.com>
2019-05-10 17:57:58 +0300

    + cloud functions

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -6,20 +6,20 @@ import {firestoreConnect} from "react-redux-firebase";
 import {compose} from "redux";
 import {Redirect} from 'react-router-dom';
 
-class Dashboard extends Component{
+class Dashboard extends Component {
     render() {
-        const {projects, auth} = this.props;
-        if(!auth.uid) {
+        const {projects, auth, notifications} = this.props;
+        if (!auth.uid) {
             return <Redirect to="/signin"/>
         }
-        return(
+        return (
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
                         <ProjectList projects={projects}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications/>
+                        <Notifications notifications={notifications}/>
                     </div>
                 </div>
             </div>
@@ -28,16 +28,18 @@ class Dashboard extends Component{
 }
 
 const mapStateToProps = (state) => {
-  return {
+    return {
 //      projects: state.project.projects
-      projects: state.firestore.ordered.projects,
-      auth: state.firebase.auth
-  }
+        projects: state.firestore.ordered.projects,
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
+    }
 };
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects'}
+        {collection: 'projects', orderBy: ['createdAt', 'desc']},
+        {collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
     ])
 )(Dashboard);
