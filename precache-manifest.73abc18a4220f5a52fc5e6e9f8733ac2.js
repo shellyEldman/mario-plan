cfccad20ab2e11567e05add43068477cd28d7195
Shelly Eldman <shelyeldman@gmail.com>
2019-07-07 20:06:59 +0300

    Updates

---
+++ precache-manifest.73abc18a4220f5a52fc5e6e9f8733ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30faafab66152befbb2d3a089bc267a",
+    "url": "/mario-plan/index.html"
+  },
+  {
+    "revision": "9da771818603c601d799",
+    "url": "/mario-plan/static/css/main.1135c735.chunk.css"
+  },
+  {
+    "revision": "7f4f54c985442fc2ab3a",
+    "url": "/mario-plan/static/js/2.a3cc4a0d.chunk.js"
+  },
+  {
+    "revision": "9da771818603c601d799",
+    "url": "/mario-plan/static/js/main.66ff6f4d.chunk.js"
+  },
+  {
+    "revision": "bace3fd149e9f65f0878",
+    "url": "/mario-plan/static/js/runtime~main.f6f1b299.js"
+  },
+  {
+    "revision": "0ab14f44a743f92e22b0e887a1ee524e",
+    "url": "/mario-plan/static/media/mario-bg.0ab14f44.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mario-plan/precache-manifest.73abc18a4220f5a52fc5e6e9f8733ac2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mario-plan/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
